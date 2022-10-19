e266f10014e81cc63c77a983e74bece3e73a67c4
Sambhav Surana <Sambhav.Surana@bottomline.com>
2022-10-19 11:58:24 +0530

    added jsdoc

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
+/**
+ * This function will take an id as parameter and returns an HTML Element refering to that id.
+ * @param {string} id The id attribute of an HTML Element
+ * @returns {HTMLElement | null} Returns a reference to the first object with the specified value of the ID attribute.
+ */
 const $ = (id) => document.getElementById(id);
+
+/**
+ * Holds all the combinations to win tic tac toe
+ * @type {number[][]} 
+ */
 const WINNING_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -10,13 +20,48 @@ const WINNING_COMBINATIONS = [
   [2, 4, 6]
 ];
 
+/**
+ * Tells whose turn it is, X or O
+ * @type {boolean}
+ */
 let xChance = true;
+
+/**
+ * Flag that describes if the game has ended
+ * @type {boolean}
+ */
 let gameOver = false;
+
+/**
+ * Holds the current value of a grid element.
+ * 0 if empty
+ * 1 if X
+ * 2 if O
+ * @type {Array}
+ */
 let elements = [0,0,0,0,0,0,0,0,0];
+
+/**
+ *  Array of reference to all elements of class gridElement
+ * @type {HTMLElement[]}
+ */
 const gridElements = document.querySelectorAll('.gridElement');
+
+/**
+ *  span tag that will display X on the webpage
+ * @type {string}
+ */
 const xSpan = "<span class=\"x\">x</span>";
+
+/**
+ *  span tag that will display O on the webpage
+ * @type {string}
+ */
 const oSpan = "<span class=\"o\">o</span>";
 
+/**
+ * A function to initialize all the global variables when the game is started or reset.
+ */
 const startGame = () => {
   gridElements.forEach((item)=> {
     item.innerHTML = '';
@@ -31,6 +76,9 @@ const startGame = () => {
 
 startGame();
 
+/**
+ * Function to check if someone has won the game. Called everytime a gridElement is clicked.
+ */
 const checkWin = () => {
   WINNING_COMBINATIONS.forEach((item) => {
     if(elements[item[0]] === elements[item[1]] && elements[item[1]] === elements[item[2]] && (elements[item[1]] === 1 || elements[item[1]] === 2)) {
@@ -42,6 +90,9 @@ const checkWin = () => {
   })
 };
 
+/**
+ * Function to check if the game is draw. Called everytime a gridElement is clicked.
+ */
 const checkDraw = () => {
   for(let ele of elements){
     if(ele === 0) return;
@@ -50,6 +101,11 @@ const checkDraw = () => {
   $('resultText').innerHTML = 'Draw...';
 };
 
+/**
+ * The function that is executed when a grid element is clicked.
+ * @param {string} index 
+ * @returns {void}
+ */
 const elementClicked = (index) => {
   let id = parseInt(index) 
   if(elements[id] !== 0) return;
